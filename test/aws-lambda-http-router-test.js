e937cc1b2ee7c3e5ed77ce0962a2dbf7ef5d410b
Vlad Khaizn <vlad.khazin@smith.co>
2018-03-04 22:20:20 +0000

    adding regex test

--- test/aws-lambda-http-router-test.js
+++ test/aws-lambda-http-router-test.js
@@ -32,166 +32,166 @@ describe('aws-lambda-http-router', function() {
   
 	describe('#GET: /', function() {
     
-		// it('Should return statusCode 200', function(done) {
+		it('Should return statusCode 200', function(done) {
       
-  //     const routes = [
-  //       {
-  //         method: 'GET',
-  //         path: '/',
-  //         handler: echoHandler
-  //       },
-  //       {
-  //         method: 'POST',
-  //         path: '/',
-  //         handler: echoHandler
-  //       }
-  //     ];
+      const routes = [
+        {
+          method: 'GET',
+          path: '/',
+          handler: echoHandler
+        },
+        {
+          method: 'POST',
+          path: '/',
+          handler: echoHandler
+        }
+      ];
       
-  //     const httpRouter		    = require('../aws-lambda-http-router').create(routes);
+      const httpRouter		    = require('../aws-lambda-http-router').create(routes);
       
-  //     httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
-  //       .then(response => {
-  //         assert.equal(response.statusCode, 200, 'Status code should be equal 200');
-  //         done();
-  //       });
-		// });
+      httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
+        .then(response => {
+          assert.equal(response.statusCode, 200, 'Status code should be equal 200');
+          done();
+        });
+		});
 
-		// it('Path missmatch 404', function(done) {
+		it('Path missmatch 404', function(done) {
       
-  //     const routes = [
-  //       {
-  //         method: 'GET',
-  //         path: '/',
-  //         handler: echoHandler
-  //       },
-  //       {
-  //         method: 'POST',
-  //         path: '/',
-  //         handler: echoHandler
-  //       }
-  //     ];
+      const routes = [
+        {
+          method: 'GET',
+          path: '/',
+          handler: echoHandler
+        },
+        {
+          method: 'POST',
+          path: '/',
+          handler: echoHandler
+        }
+      ];
       
-  //     const httpRouter		    = require('../aws-lambda-http-router').create(routes);
+      const httpRouter		    = require('../aws-lambda-http-router').create(routes);
       
-  //     const echoEvent = {
-  //       resource: "/",
-  //       path: "/echo",
-  //       httpMethod: "GET",
-  //       body: null,
-  //     };
+      const echoEvent = {
+        resource: "/",
+        path: "/echo",
+        httpMethod: "GET",
+        body: null,
+      };
       
-  //     httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
-  //       .then(response => {
-  //         assert.equal(response.statusCode, 404, 'Status code should be equal 404');
-  //         done();
-  //       });
-		// });
+      httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
+        .then(response => {
+          assert.equal(response.statusCode, 404, 'Status code should be equal 404');
+          done();
+        });
+		});
     
-  //   it('Method missmatch 404', function(done) {
-  //       const routes = [
-  //         {
-  //           method: 'POST',
-  //           path: '/echo',
-  //           handler: echoHandler
-  //         }
-  //       ];
+    it('Method missmatch 404', function(done) {
+        const routes = [
+          {
+            method: 'POST',
+            path: '/echo',
+            handler: echoHandler
+          }
+        ];
 
-  //       const httpRouter		    = require('../aws-lambda-http-router').create(routes);
+        const httpRouter		    = require('../aws-lambda-http-router').create(routes);
 
-  //       const echoEvent = {
-  //         resource: "/",
-  //         path: "/echo",
-  //         httpMethod: "GET",
-  //         body: null,
-  //       };
+        const echoEvent = {
+          resource: "/",
+          path: "/echo",
+          httpMethod: "GET",
+          body: null,
+        };
 
-  //       httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
-  //         .then(response => {
-  //           assert.equal(response.statusCode, 404, 'Status code should be equal 404');
-  //           done();
-  //         });
-  //     });    
+        httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
+          .then(response => {
+            assert.equal(response.statusCode, 404, 'Status code should be equal 404');
+            done();
+          });
+      });    
 
-  //   it('Multi-match 500', function(done) {
+    it('Multi-match 500', function(done) {
 
-  //     const routes = [
-  //       {
-  //         method: 'GET',
-  //         path: '/echo',
-  //         handler: echoHandler
-  //       },
-  //       {
-  //         method: 'GET',
-  //         path: '/echo',
-  //         handler: echoHandler
-  //       }
-  //     ];
+      const routes = [
+        {
+          method: 'GET',
+          path: '/echo',
+          handler: echoHandler
+        },
+        {
+          method: 'GET',
+          path: '/echo',
+          handler: echoHandler
+        }
+      ];
 
-  //     const httpRouter		    = require('../aws-lambda-http-router').create(routes);
+      const httpRouter		    = require('../aws-lambda-http-router').create(routes);
 
-  //     const echoEvent = {
-  //       resource: "/",
-  //       path: "/echo",
-  //       httpMethod: "GET",
-  //       body: null,
-  //     };
+      const echoEvent = {
+        resource: "/",
+        path: "/echo",
+        httpMethod: "GET",
+        body: null,
+      };
 
-  //     httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
-  //       .then(response => {
-  //         assert.equal(response.statusCode, 500, 'Status code should be equal 500');
-  //         done();
-  //       });
-  //   });
+      httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
+        .then(response => {
+          assert.equal(response.statusCode, 500, 'Status code should be equal 500');
+          done();
+        });
+    });
     
-  //   it('Body should be string', function(done) {
+    it('Body should be string', function(done) {
       
-  //     const routes = [
-  //       {
-  //         method: 'GET',
-  //         path: '/',
-  //         handler: echoHandler
-  //       },
-  //       {
-  //         method: 'POST',
-  //         path: '/',
-  //         handler: echoHandler
-  //       }
-  //     ];
+      const routes = [
+        {
+          method: 'GET',
+          path: '/',
+          handler: echoHandler
+        },
+        {
+          method: 'POST',
+          path: '/',
+          handler: echoHandler
+        }
+      ];
       
-  //     const httpRouter		    = require('../aws-lambda-http-router').create(routes);
+      const httpRouter		    = require('../aws-lambda-http-router').create(routes);
       
-  //     httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
-  //       .then(response => {
-  //         assert.equal(typeof(response.body), 'string', 'Body should be string');
-  //         done();
-  //       });
-		// });
+      httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
+        .then(response => {
+          assert.equal(typeof(response.body), 'string', 'Body should be string');
+          done();
+        });
+		});
 		
-		// it('Method missmatch: 404', function(done) {
+		it('Method missmatch: 404', function(done) {
       
-  //     const routes = [
-  //       {
-  //         method: 'GET',
-  //         path: '/echo',
-  //         handler: echoHandler
-  //       }
-  //     ];
+      const routes = [
+        {
+          method: 'GET',
+          path: '/echo',
+          handler: echoHandler
+        }
+      ];
       
-  //     const httpRouter		    = require('../aws-lambda-http-router').create(routes);
+      const httpRouter		    = require('../aws-lambda-http-router').create(routes);
       
-  //     const echoEvent = {
-  //       resource: "/",
-  //       path: "/echo",
-  //       httpMethod: "PUT",
-  //       body: null,
-  //     };
+      const echoEvent = {
+        resource: "/",
+        path: "/echo",
+        httpMethod: "PUT",
+        body: null,
+      };
       
-  //     httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
-  //       .then(response => {
-  //         assert.equal(response.statusCode, 404, 'Status code should be equal 404');
-  //         done();
-  //       });
-		// });
+      httpRouter.handler(echoEvent, lambdaContext, lambdaCallback)
+        .then(response => {
+          assert.equal(response.statusCode, 404, 'Status code should be equal 404');
+          done();
+        });
+		});
 		
 		it('Should return statusCode 200 for RegEx', function(done) {
       
